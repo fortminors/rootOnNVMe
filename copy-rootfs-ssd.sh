886629c0ef281ff3ea22cec64ed8bda72becbb0b
Georgy Gunkin <fortminors@yandex.ru>
2022-05-12 13:57:38 +0300

    fix: creating a folder for mount and specifying type ext4 during mounting

--- copy-rootfs-ssd.sh
+++ copy-rootfs-ssd.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# Create a folder for mount
+sudo mkdir -p /media/ssd
+
 # Mount the SSD as /mnt
-sudo mount /dev/nvme0n1p1 /mnt
+sudo mount -t ext4 /dev/nvme0n1p1 /media/ssd
 # Copy over the rootfs from the SD card to the SSD
 sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/","/media/*","/lost+found"} / /mnt
 # We want to keep the SSD mounted for further operations
